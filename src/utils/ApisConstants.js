c8cdd4a5fc0c2a29dd41d29a6151ded3d2baed97
Farhan Khan <khanfarhan1901@gmail.com>
2025-09-16 00:21:58 +0530

    get and update user profile api constants added

--- src/utils/ApisConstants.js
+++ src/utils/ApisConstants.js
@@ -3,6 +3,8 @@ const baseUrl = "http://localhost:5000/api";
 export const LOGIN_API = `${baseUrl}/auth/login`;
 export const LOGOUT_API = `${baseUrl}/auth/logout`;
 export const REGISTER_API = `${baseUrl}/auth/register`;
+export const GET_PROFILE_API = (id) => `${baseUrl}/auth/get-profile/${id}`;
+export const UPDATE_PROFILE_API = (id) => `${baseUrl}/auth/update-profile/${id}`;
 
 export const createPostAPI = `${baseUrl}/posts/create-post`;
 export const getAllPostsAPI = `${baseUrl}/posts/all-posts`;
